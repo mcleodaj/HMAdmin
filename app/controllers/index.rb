5b10159708b321d2ba0c71955201acb9a13fd0a9
mcleodaj <mcleoda@yahoo.com>
2019-03-10 12:58:29 -0400

    Test session persistence lines

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -22,6 +22,8 @@ end
 
 get '/' do
   cache_control :public, :max_age => 31536000
+  session.delete 'init'
+  puts session.inspect.to_s
   unless session[:user_id] != nil
     redirect "login"
   else
@@ -31,6 +33,7 @@ end
 
 get '/price', :auth => :user do
   cache_control :public, :max_age => 31536000
+  session.delete 'init'
   file='public/gas_price.txt'
   File.readlines(file).each do |line|
     @gas_price = line
@@ -40,19 +43,24 @@ end
 
 post '/price', :auth => :user do
   cache_control :public, :max_age => 31536000
+  session.delete 'init'
   File.open("public/gas_price.txt", "w") { |file| file.puts params[:gas_price]}
   redirect '/price'
 end
 
 get '/login' do
+  puts session.inspect.to_s
   cache_control :public, :max_age => 31536000
+  session.delete 'init'
   erb :login
 end
 
 post '/login' do
   user = User.find_by(:username => params[:username])
+  session.delete 'init'
   if user && user.authenticate(params[:password])
     session[:user_id] = user.id
+    puts session.inspect.to_s
     redirect "/"
   else
     redirect "/login"
@@ -61,6 +69,7 @@ end
 
 post '/logout' do
   cache_control :public, :max_age => 31536000
+  session.delete 'init'
   session[:user_id] = nil
   session.clear
   redirect "/"
